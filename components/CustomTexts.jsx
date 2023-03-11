640dd0ae9b96885c1c5822d1cadb7d2780029fa1
gracebir <grace.devolop@gmail.com>
2023-03-11 23:36:51 +0200

    typing text

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,22 @@
 'use client';
+import { motion } from 'framer-motion'
+import { 
+  textContainer, textVariant2
+} from '../utils/motion';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+export const TypingText = ({title, textStyles}) => (
+  <motion.p 
+  variants={textContainer}
+  className={`${textStyles} font-normal text-[0.875rem] text-secondary-white`}
+  >
+    {Array.from(title).map((letter, index)=> (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0': letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({title, textStyles}) => (
   <h2>Title Text</h2>
 );
