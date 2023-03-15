e1a61e9750ae86e0bcc00f23dca9187198101045
gracebir <grace.devolop@gmail.com>
2023-03-15 11:19:11 +0200

    create world's section header

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,24 @@
 'use client';
+import { motion } from 'framer-motion'
+import { TypingText, TitleText, StartSteps } from '../components'
+import styles from '../styles';
+import { 
+  fadeIn,
+  planetVariants,
+  staggerContainer,
+} from '../utils/motion';
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings}`}>
+    <motion.div 
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once: false, amount: 0.25}}
+     className={`${styles.innerWidth} mx-auto`}>
+      <TypingText title="| People on the World" textStyles="text-center"/>
+      <TitleText title={<>Track friends around you and <br className='hidden lg:block'/> invite them to play together in<br className='hidden lg:block'/> the same world</>} textStyles="text-center"/>
+    </motion.div>
   </section>
 );
 
